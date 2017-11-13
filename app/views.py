a7047992c1cf14b05e0f8a5eeb5adc80d204f889
Michael Gingras <mcg79@cornell.edu>
2017-11-13 10:16:00 -0500

    initial commit

--- app/views.py
+++ app/views.py
@@ -2,6 +2,29 @@ from app import app
 from flask import render_template
 
 @app.route('/')
-@app.route('/index')
 def index():
     return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/draw')
+def draw():
+    return render_template('draw.html')
+
+@app.route('/tattoo')
+def tat():
+    return render_template('tattoo.html')
+
+@app.route('/submissions')
+def submissions():
+    return render_template('submissions.html')
+
+@app.route('/live')
+def live():
+    return render_template('live.html')
+
+@app.route('/visualize')
+def vis():
+    return render_template('visualize.html')
